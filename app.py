83d9fdabb293fb83c6cafa0b3b2785e5c596fefa
sahilawatramani <155528484+sahilawatramani@users.noreply.github.com>
2025-07-18 16:37:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,29 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from gradio_client import Client
-import requests
 
 app = Flask(__name__)
 client = Client("sahilawatramani/crime-analytics-backend")
+
 @app.route("/api/gr_query", methods=["POST"])
 def gr_query():
     query = request.json.get("query")
-    response = client.predict(query,api_name="/gr_query")
-    
-    return response
+    response = client.predict(query, api_name="/gr_query")
+    return jsonify(response)
 
 @app.route("/api/gr_quick_stats", methods=["GET"])
 def gr_quick_stats():
     response = client.predict(api_name="/gr_quick_stats")
-    return response
+    return jsonify(response)
 
 @app.route("/api/gr_filter_options", methods=["GET"])
 def gr_filter_options():
     response = client.predict(api_name="/gr_filter_options")
-    return response
+    return jsonify(response)
 
 @app.route("/api/gr_sample_queries", methods=["GET"])
 def gr_sample_queries():
-    response = result = client.predict(api_name="/gr_sample_queries")
-    return response
+    response = client.predict(api_name="/gr_sample_queries")
+    return jsonify(response)
 
 if __name__ == "__main__":
-    app.run(debug=True) 
+    app.run(debug=True)
