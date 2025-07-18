e4ae9014f354853e3f28bed3732b84e196dbbca8
sahilawatramani <155528484+sahilawatramani@users.noreply.github.com>
2025-07-18 16:09:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,29 @@
 from flask import Flask, request, jsonify
+from gradio_client import Client
 import requests
 
 app = Flask(__name__)
-GRADIO_BACKEND_URL = "https://sahilawatramani-crime-analytics-backend.hf.space"
-
+client = Client("sahilawatramani/crime-analytics-backend")
 @app.route("/api/gr_query", methods=["POST"])
 def gr_query():
-    user_query = request.json.get("query")
-    response = requests.post(
-        f"{GRADIO_BACKEND_URL}/gr_query",
-        json={"query": user_query}
-    )
+    query = request.json.get("query")
+    response = client.predict(query,api_name="/gr_query")
+    
     return jsonify(response.json()), response.status_code
 
 @app.route("/api/gr_quick_stats", methods=["GET"])
 def gr_quick_stats():
-    response = requests.get(f"{GRADIO_BACKEND_URL}/gr_quick_stats")
+    response = client.predict(api_name="/gr_quick_stats")
     return jsonify(response.json()), response.status_code
 
 @app.route("/api/gr_filter_options", methods=["GET"])
 def gr_filter_options():
-    response = requests.get(f"{GRADIO_BACKEND_URL}/gr_filter_options")
+    response = client.predict(api_name="/gr_filter_options")
     return jsonify(response.json()), response.status_code
 
 @app.route("/api/gr_sample_queries", methods=["GET"])
 def gr_sample_queries():
-    response = requests.get(f"{GRADIO_BACKEND_URL}/gr_sample_queries")
+    response = result = client.predict(api_name="/gr_sample_queries")
     return jsonify(response.json()), response.status_code
 
 if __name__ == "__main__":
