f6f27d4bf6b507b21534929fa139873ffcc87d31
sahilawatramani <155528484+sahilawatramani@users.noreply.github.com>
2025-07-18 16:15:46 +0530

    Update app.py to fix error
    The error of not being able to show output being fixed by changing the post, get with gradio api

--- app.py
+++ app.py
@@ -9,22 +9,22 @@ def gr_query():
     query = request.json.get("query")
     response = client.predict(query,api_name="/gr_query")
     
-    return jsonify(response.json()), response.status_code
+    return jsonify(response), response.status_code
 
 @app.route("/api/gr_quick_stats", methods=["GET"])
 def gr_quick_stats():
     response = client.predict(api_name="/gr_quick_stats")
-    return jsonify(response.json()), response.status_code
+    return jsonify(response), response.status_code
 
 @app.route("/api/gr_filter_options", methods=["GET"])
 def gr_filter_options():
     response = client.predict(api_name="/gr_filter_options")
-    return jsonify(response.json()), response.status_code
+    return jsonify(response), response.status_code
 
 @app.route("/api/gr_sample_queries", methods=["GET"])
 def gr_sample_queries():
     response = result = client.predict(api_name="/gr_sample_queries")
-    return jsonify(response.json()), response.status_code
+    return jsonify(response), response.status_code
 
 if __name__ == "__main__":
     app.run(debug=True) 
